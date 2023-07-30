67015e183cafdebf6dab25ed3cd76d66f14da86e
Serhii <vasylenkoserhiiv@gmail.com>
2023-07-30 13:51:59 +0300

    HW3

---
+++ src/Task_1/ClassRoom.java
@@ -0,0 +1,18 @@
+package Task_1;
+/*
+Створити клас, який представляє навчальний клас ClassRoom. Створіть клас учень Pupil.
+У тілі класу створіть методи void study(), void read(), void write(), void relax().
+Створіть 3 похідні класу ExcellentPupil, GoodPupil, BadPupil від класу базового класу Pupil і
+перевизначте кожен із методів, залежно від успішності учня. Конструктор класу ClassRoom приймає
+аргументи типу Pupil, клас має складатися із 4 учнів. Передбачте можливість, що користувач може
+передати 2 або 3 аргументи. Виведіть інформацію про те, як усі учні екземпляра класу ClassRoom
+вміють навчатися, читати, писати, відпочивати.
+ */
+public class ClassRoom {
+    private Pupil pupil_1;
+    private Pupil pupil_2;
+    private Pupil pupil_3;
+    private Pupil pupil_4;
+
+
+}
